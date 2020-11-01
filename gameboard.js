8d57201dc9b4244e0ab3b7b2f3d81c93503d7aed
Shawn <codingshawn@gmail.com>
2020-11-01 21:44:16 +0800

    Simplified displayboard function logic

--- gameboard.js
+++ gameboard.js
@@ -1,5 +1,5 @@
 const gameboard = (() => {
-    let markerPositions = ["x","x","x","x","o","","x","x","x"];
+    let markerPositions = ["","","","","","","","",""];
     let board = document.getElementById("gameboard");
     let gamesquares = board.getElementsByTagName("div");
     
@@ -17,17 +17,14 @@ const gameboard = (() => {
 
     const displayBoard = () => {
         for (i = 0; i < markerPositions.length; i++) {
-            if (markerPositions[i] === "x") {
-                gamesquares[i].textContent = "X";
-            } else if (markerPositions[i] === "o") {
-                gamesquares[i].textContent = "O";
-            }
+            gamesquares[i].textContent = markerPositions[i];
         }
     }
 
     const addMarker = (index) => {
         if (markerPositions[index] === "") {
             markerPositions[index] = currentPlayer.marker;
+            events.publish('markerPlaced', markerPositions);
         } else {
             alert("Please choose another spot!");
         }
