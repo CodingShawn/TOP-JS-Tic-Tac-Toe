2c2f7581cfe96ab4201e2efb6ef9fea9407d7652
Shawn <codingshawn@gmail.com>
2020-11-01 21:44:55 +0800

    Added player factory function

---
+++ player.js
@@ -0,0 +1,11 @@
+const player = (symbol) => {
+    
+    const marker = symbol;
+
+    return {marker};
+}
+
+const player1 = player("X");
+const player2 = player("Y");
+
+var currentPlayer = player1;
