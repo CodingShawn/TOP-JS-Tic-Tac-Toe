7c91fe933d77ee9021c9c28fba01e0554ea8429c
Shawn <codingshawn@gmail.com>
2020-11-01 20:57:43 +0800

    Added event listener to gameboard to allow players to place marker

--- gameboard.js
+++ gameboard.js
@@ -2,6 +2,18 @@ const gameboard = (() => {
     let markerPositions = ["x","x","x","x","o","","x","x","x"];
     let board = document.getElementById("gameboard");
     let gamesquares = board.getElementsByTagName("div");
+    
+    board.addEventListener('click', function(e) {
+        if (e.target && e.target.nodeName === "DIV") {
+            let index = 0;
+            while (e.target.parentNode.children[index] !== e.target) {
+                index ++;
+            }
+            addMarker(index);
+            displayBoard();
+        }
+    })
+
 
     const displayBoard = () => {
         for (i = 0; i < markerPositions.length; i++) {
@@ -12,6 +24,15 @@ const gameboard = (() => {
             }
         }
     }
-    return {displayBoard, board}
+
+    const addMarker = (index) => {
+        if (markerPositions[index] === "") {
+            markerPositions[index] = currentPlayer.marker;
+        } else {
+            alert("Please choose another spot!");
+        }
+    }
+
+    return {displayBoard, board, gamesquares}
 
 })();
