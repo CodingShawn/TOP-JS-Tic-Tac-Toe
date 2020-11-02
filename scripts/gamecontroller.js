cc574e99c2faa99ccd8acea9a452e1b5e52417a2
Shawn <codingshawn@gmail.com>
2020-11-02 22:03:13 +0800

    Added win conditions checking and alert player upon winning

--- scripts/gamecontroller.js
+++ scripts/gamecontroller.js
@@ -13,6 +13,46 @@ const gamecontroller = (() => {
 
     const getCurrentPlayer = () => currentPlayer
 
+
+
+    const checkIfWin = (markerPositions) => {
+        //Using centre cell as common point
+        if (((markerPositions[4] === currentPlayer.marker) && 
+            //If diagonals are 3 markers in a row
+            (markerPositions[0] === currentPlayer.marker
+          && markerPositions[8] === currentPlayer.marker) || 
+            (markerPositions[2] === currentPlayer.marker
+          && markerPositions[6] === currentPlayer.marker) ||
+            //If vertical through centre are 3 in a row
+            (markerPositions[1] === currentPlayer.marker
+          && markerPositions[7] === currentPlayer.marker) ||
+            //If horizontal through centre are 3 in a row
+            (markerPositions[3] === currentPlayer.marker
+          && markerPositions[5] === currentPlayer.marker)) ||
+            //Using first cell as common point
+            ((markerPositions[0] === currentPlayer.marker) 
+          &&
+            //First row are 3 in a row
+            ((markerPositions[1] === currentPlayer.marker
+          && markerPositions[2] === currentPlayer.marker) ||
+            //First column are 3 in a row
+            (markerPositions[3] === currentPlayer.marker 
+          && markerPositions[6] === currentPlayer.marker))) ||
+            //Using last cell as common point
+            ((markerPositions[8] === currentPlayer.marker)
+            //Last column are 3 in a row 
+          && ((markerPositions[2] === currentPlayer.marker
+          && markerPositions[5] === currentPlayer.marker) ||
+            //Last row are 3 in a row
+            (markerPositions[6] === currentPlayer.marker
+          && markerPositions[7] === currentPlayer.marker)))) {
+            //Actual function starts here
+            alert(currentPlayer.marker + " has won!");
+        }
+    }
+
+
+    pubsub.subscribe('markerPlaced', checkIfWin);
     pubsub.subscribe('markerPlaced', changePlayer);
     
     return {changePlayer, getCurrentPlayer};  
